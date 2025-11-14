ce98875520599a0d5ef99913b4cadee0ca7a5472
diegotosato <tosato1998@gmail.com>
2025-11-14 13:21:01 +0100

    create container and row for better layout

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -18,29 +18,39 @@ export default function Products() {
     return (
         <>
             <h1>Prodotti</h1>
-            {
-                products.map(product => (
-                    <div key={product.id} className="card">
 
-                        <div className="card-image">
-                            <img src={product.image} alt={product.title} />
-                        </div>
+            <div className="container">
 
-                        <div className="card-body">
-                            <h5 className="card-title">{product.title}</h5>
-                            <h6 className="card-subtitle mb-2 text-body-secondary">{product.category}</h6>
-                            <p className="card-text">{product.description}</p>
-                            <h5 className="card-title">€ {product.price}</h5>
-                            <p className="card-text">
-                                <span>Rating: {product.rating.rate}</span>
-                                <span>Count: {product.rating.count}</span>
-                            </p>
-                        </div>
+                <div className="row row-cols-4 g-3">
 
-                    </div>
-                ))
-            }
+                    {
+                        products.map(product => (
+                            <div key={product.id} className="col">
+                                <div className="card h-100">
 
+                                    <div className="card-image">
+                                        <img className="card-img-top" src={product.image} alt={product.title} />
+                                    </div>
+
+                                    <div className="card-body">
+                                        <h5 className="card-title">{product.title}</h5>
+                                        <h6 className="card-subtitle mb-2 text-body-secondary">{product.category}</h6>
+                                        <p className="card-text">{product.description}</p>
+                                        <h5 className="card-title">€ {product.price}</h5>
+                                        <p className="card-text">
+                                            <span>Rating: {product.rating.rate}</span>
+                                            <span>Count: {product.rating.count}</span>
+                                        </p>
+                                    </div>
+
+                                </div>
+                            </div>
+                        ))
+                    }
+
+                </div>
+
+            </div>
 
         </>
     )
