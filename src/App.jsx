df8385d1c98768ee8e98fe2e21f823775868fc59
diegotosato <tosato1998@gmail.com>
2025-11-14 12:11:01 +0100

    create three components for the principals pages ofthe app, import them in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import HomePage from './Pages/HomePage'
+import Products from './Pages/Products'
+import AboutUs from './Pages/AboutUs'
+
 function App() {
 
   return (
     <>
-      <h1>Worka</h1>
+      <HomePage />
+      <Products />
+      <AboutUs />
     </>
   )
 }
