10c2c480a1ca215cac9a6db4a77b135633ea34d6
diegotosato <tosato1998@gmail.com>
2025-11-17 13:58:32 +0100

    adjust focus product layout

--- src/Pages/FocusProduct.jsx
+++ src/Pages/FocusProduct.jsx
@@ -5,7 +5,7 @@ import axios from "axios"
 export default function FocusProduct() {
 
     const { id } = useParams()
-    const [product, setProduct] = useState([])
+    const [product, setProduct] = useState({ rating: {} })
 
     function handleGet() {
         axios.get(`https://fakestoreapi.com/products/${id}`)
@@ -14,31 +14,34 @@ export default function FocusProduct() {
             })
     }
 
-    useEffect(handleGet, [])
+    useEffect(handleGet, [id])
     console.log(product.rating);
 
 
     return (
         <>
             <div className="container">
+
+                <h1 className="my-4">{product.title}</h1>
+
                 <div className="card mb-3 h-100">
                     <div className="row g-0 h-100">
-                        <div className="col-5 d-flex align-items-center p-5">
+                        <div className="col-6 d-flex align-items-center p-5">
                             <img src={product.image} className="img-fluid rounded-start" alt={product.title} />
                         </div>
-                        <div className="col-7 h-100">
+                        <div className="col-6">
                             <div className="card-body p-3 h-100 d-flex flex-column justify-content-center">
-                                <h5 className="card-title text-primary">{product.title}</h5>
-                                <h6 className="card-subtitle mb-2 text-body-secondary">{product.category}</h6>
+                                <h5 className="card-title text-primary fs-1">{product.title}</h5>
+                                <h6 className="card-subtitle mb-2 text-body-secondary fs-3">{product.category}</h6>
                                 <p className="card-text">{product.description}</p>
-                                <h5 className="card-title text-success">€ {product.price}</h5>
+                                <h5 className="card-title text-success fs-3">€ {product.price}</h5>
                                 <p className="card-text">Rating: <span className="text-warning fw-bold">{product.rating.rate}</span></p>
-                                {/* <p className="card-text">Count: <span className="text-warning fw-bold">{product.rating.count}</span></p> */}
+                                <p className="card-text">Count: <span className="text-warning fw-bold">{product.rating.count}</span></p>
+                                <Link to='/products' className="btn btn-primary w-50">Torna alla pagina dei prodotti</Link>
                             </div>
                         </div>
                     </div>
                 </div>
-                <Link to='/products' className="btn btn-primary">Torna alla pagina dei prodotti</Link>
             </div>
         </>
     )
